cd8a57667033fe08d6cf10c473a81234e94b20c2
krpriyevart <kumarpriyevart.com>
2024-04-16 20:13:57 +0530

    all setup config

--- src/app.js
+++ src/app.js
@@ -10,4 +10,10 @@ app.use(express.json({limit:"16KB"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//user routes
+import userRouter from './routes/user.routes.js'
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
 export default app 
