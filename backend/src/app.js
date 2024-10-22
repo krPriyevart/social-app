9c226eaca0ca3eb483a5588d2acc45e03571ba84
krpriyevart <kumarpriyevart.com>
2024-10-22 08:45:31 +0530

    a

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,10 +2,10 @@ import express from "express"
 import cors from "cors"  
 import cookieParser from "cookie-parser"
 const app = express()
-// app.use(cors({
-//     origin: ['https://social-app-frontend-tan.vercel.app'],
-//     credentials: true
-// }))
+app.use(cors({
+    origin: ['https://social-app-frontend-tan.vercel.app'],
+    credentials: true
+}))
 app.use(express.json({limit:"16KB"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
