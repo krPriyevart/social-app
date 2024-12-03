6b83b63342f689022c1cdd80d6ca18344fdbae1d
krpriyevart <kumarpriyevart.com>
2024-12-04 01:56:45 +0530

    a

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,29 @@
 import express from "express"
-import { Router } from "express";
 import cors from "cors"  
 import cookieParser from "cookie-parser"
 const app = express()
-const router = Router()
+
+// app.use(cors({
+//     origin: '*',
+//     credentials: true
+// }))
+
+const allowedOrigins = [
+    'http://localhost:5173', 
+    'https://social-app-frontend-tan.vercel.app', 
+];
+
 app.use(cors({
-    origin: '*',
-    credentials: true
-}))
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+}));
+
 app.use(express.json({limit:"16KB"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
